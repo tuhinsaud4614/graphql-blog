3bc3baf0c82e0ddc86541834444f20fba6d0d438
tuhinsaud4614@gmail.com <tuhinsaud4614@gmail.com>
2022-08-08 05:02:18 +0600

    Controller:User
    User Query controller created

--- backend/src/resolvers/user/query.resolver.ts
+++ backend/src/resolvers/user/query.resolver.ts
@@ -1,8 +1,28 @@
+import { GraphQLYogaError } from "@graphql-yoga/node";
+import { getUserById } from "../../services/user.service";
+import { NOT_EXIST_ERR_MSG } from "../../utils/constants";
 import { YogaContextReturnType } from "../../utils/types";
+import { getGraphqlYogaError } from "../../validations";
 
 export const Query = {
   async users(_: unknown, __: unknown, { prisma }: YogaContextReturnType) {
     const u = await prisma.user.findMany();
     return u;
+  },
+
+  async user(
+    _: unknown,
+    { id }: { id: string },
+    { prisma }: YogaContextReturnType
+  ) {
+    try {
+      const user = await getUserById(prisma, id);
+      if (user === null) {
+        return new GraphQLYogaError(NOT_EXIST_ERR_MSG("User"));
+      }
+      return user;
+    } catch (error) {
+      return getGraphqlYogaError(error, NOT_EXIST_ERR_MSG("User"));
+    }
   },
 };
