a8267be35784d8fb53feebe2c477e9b2ae801524
Md. Tuhin Saud <tuhinsaud4614@gmail.com>
2023-05-02 21:54:47 +0600

    post subscription updated

--- backend/src/resolvers/post/subscription.resolver.ts
+++ backend/src/resolvers/post/subscription.resolver.ts
@@ -1,13 +1,9 @@
-import { GraphQLError, GraphQLResolveInfo } from "graphql";
+import { GraphQLResolveInfo } from "graphql";
 
 import logger from "@/logger";
-import {
-  SUBSCRIPTION_REACTIONS_ERR_MSG,
-  UN_AUTH_ERR_MSG,
-  UN_AUTH_EXT_ERR_CODE,
-} from "@/utils/constants";
+import { AuthenticationError, UnknownError } from "@/model";
+import { SUBSCRIPTION_REACTIONS_ERR_MSG } from "@/utils/constants";
 import { YogaContext } from "@/utils/types";
-import { getGraphqlYogaError } from "@/validations";
 
 export const Subscription = {
   reactions: {
@@ -19,17 +15,13 @@ export const Subscription = {
     ) {
       try {
         if (user === null) {
-          return new GraphQLError(UN_AUTH_ERR_MSG, {
-            extensions: {
-              code: UN_AUTH_EXT_ERR_CODE,
-            },
-          });
+          return new AuthenticationError();
         }
         // return pubSub.subscribe(SUBSCRIPTION_REACTIONS(postId));
         return pubSub.subscribe("reactions", postId);
       } catch (error) {
         logger.error(error);
-        return getGraphqlYogaError(error, SUBSCRIPTION_REACTIONS_ERR_MSG);
+        return new UnknownError(SUBSCRIPTION_REACTIONS_ERR_MSG);
       }
     },
     resolve: (payload: unknown) => payload,
