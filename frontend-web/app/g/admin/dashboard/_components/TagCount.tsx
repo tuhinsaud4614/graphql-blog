96522f47f3c2906c70aaccf79c5346b8cb32d132
Md. Tuhin Saud <tuhinsaud4614@gmail.com>
2023-11-11 22:33:44 +0600

    Add tag count value in dashboard

--- frontend-web/app/g/admin/dashboard/_components/TagCount.tsx
+++ frontend-web/app/g/admin/dashboard/_components/TagCount.tsx
@@ -1,13 +1,25 @@
+"use client";
+
 import { Tag } from "lucide-react";
+import { toast } from "sonner";
+
+import { useGetTagCountQuery } from "@/graphql/generated/schema";
 
 import AdminDashboardCard from "./Card";
 
 export default function AdminDashboardTagCount() {
+  const { data, loading } = useGetTagCountQuery({
+    fetchPolicy: "network-only",
+    errorPolicy: "all",
+    onError(error) {
+      toast.error(error.message, { position: "bottom-right" });
+    },
+  });
   return (
     <AdminDashboardCard
       icon={<Tag className="h-8 w-8 text-primary" />}
       title="Tags"
-      value={100}
+      value={loading ? undefined : data?.tagCount || 0}
     />
   );
 }
